6c46a8218415a903ea02b876180908043d8f7ccc
Mina7o <pranay.yadav@euclidinnovations.com>
2026-05-14 19:42:31 +0000

    Use PORT env var for deployment

--- app.py
+++ app.py
@@ -179,4 +179,5 @@ def enrich_batch():
     return jsonify({'enriched': len(results), 'results': results})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
